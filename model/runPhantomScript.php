6c7cd19e85a1cced1ee2deae8b3a968c031904aa
root <root@template.ovh>
2017-08-17 16:56:57 +0200

    test

--- model/runPhantomScript.php
+++ model/runPhantomScript.php
@@ -1,11 +1,11 @@
 <?php
 
 
-include("phantomScript.js");
+//include("phantomScript.js");
 // We suppose that the variable $theme is already on the format of a query (james+bond+daniel+craig for example)
 
 function runPhantomScript($theme) {
-    $runScriptCommand = 'phantomjs scriptGetPictureUrl.js '.'"'.$theme.'"';
+    $runScriptCommand = 'phantomjs phantomScript.js '.'"'.$theme.'"';
     // Here we run the phantomJS script (assuming phantom is correctly installed on the vps) and we store the logs in a variable
     $logScriptPhantom = shell_exec($runScriptCommand);
 
@@ -17,10 +17,10 @@ function runPhantomScript($theme) {
     // the following variable contains the number of images found
     $pictureUrlsSize = count($pictureUrls);
 
-    return $pictureUrls;
+    return $runScriptCommand;
 }
 
 
 // test the function :
 $testResult = runPhantomScript("james+bond+daniel+craig");
-var_dump($testResult);
+echo $testResult;
