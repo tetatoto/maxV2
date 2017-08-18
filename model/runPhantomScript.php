d5a4bf7add62f1a5bb10a2e2ce6de676a2c84422
DEBAUCHE Theophile DTSI/DSI <LJSR3076@orange.com>
2017-08-18 11:47:54 +0200

    cleaning run script phantom

--- model/runPhantomScript.php
+++ model/runPhantomScript.php
@@ -14,6 +14,7 @@ function runPhantomScript($theme) {
     $giving_auth2 = shell_exec('chmod 777 outputs/*');
     $pictureUrls = array();
 
+    // I made several tries because sometimes the first script run is always failing
     while ($nbTry < 5 && $currentSize < $wantedSize) {
         // Here we run the phantomJS script (assuming phantom is correctly installed on the vps) and we store the logs in a variable
         $logScriptPhantom = shell_exec($runScriptCommand);
@@ -25,30 +26,22 @@ function runPhantomScript($theme) {
         $pictureUrlsSize = count($pictureUrls);
 
         $currentSize = $pictureUrlsSize;
-        echo '<p>wanted size = '.$wantedSize.'</p>';
-        echo '<p>nb try = '.$nbTry.'</p>';
+
+        // FOR TESTING :
+        // echo '<p>current size = '.$currentSize.'</p>';
+        // echo '<p>nb try = '.$nbTry.'</p>';
         $nbTry++;
     }
 
-    // // Here we run the phantomJS script (assuming phantom is correctly installed on the vps) and we store the logs in a variable
-    // $logScriptPhantom = shell_exec($runScriptCommand);
+    // NB : SO WE NEED THE LOGS (and the console.log(url[i]) etc) TO GET THE URLS INSIDE PHP VARIABLES
 
-    // // Then we parse the logs to get the url of the images and we store them in a variable
-    // $pictureUrls = preg_split("/[^\w]*([\s]+[^\w]*|$)/", $logScriptPhantom, -1, PREG_SPLIT_NO_EMPTY);
-
-    // // NB : SO WE NEED THE LOGS (and the console.log(url[i]) etc) TO GET THE URLS INSIDE PHP VARIABLES
-
-    // // the following variable contains the number of images found
-    // $pictureUrlsSize = count($pictureUrls);
 
     return $pictureUrls;
 }
 
 
-// test the function :
-$testResult1 = runPhantomScript("++++++++++++Avignon+:+le+réseau+Orange+victime+d'une+coupure++++++++");
-var_dump( $testResult1);
-echo 'booo';
-echo 'boo';
-echo '4';
+// FOR TESTING the function :
+// $testResult1 = runPhantomScript("++++++++++++Avignon+:+le+réseau+Orange+victime+d'une+coupure++++++++");
+// var_dump( $testResult1);
+
 
