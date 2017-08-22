9971c15d9b528e4a1f1fe8e4a605fe1e8b4e4c7c
DEBAUCHE Theophile DTSI/DSI <LJSR3076@orange.com>
2017-08-22 11:45:52 +0200

    script ...

--- model/videoCreationScript.sh
+++ model/videoCreationScript.sh
@@ -22,16 +22,16 @@ echo "_______________________________________________________________"
 
 while [ $current_duration -lt $video_duration ]
 do 
-	printf "file '%s'\n" templates/template_woman.mp4 >> mylist.txt;
+	printf "file '%s'\n" model/outputs/template_woman.mp4 >> mylist.txt;
 	((current_duration=current_duration+template_duration))
 	#echo "current duration is $current_duration"
 
 done
 
-ffmpeg -f concat -i mylist.txt -c copy video_outputs/generated_video.mp4
+ffmpeg -f concat -i mylist.txt -c copy model/outputs/generated_video_step1.mp4
 
 echo "final duration is $current_duration"
 
 # 1.2 // Adding the sound file to the video
 
-ffmpeg -i "video_outputs/generated_video.mp4" -i "audio_outputs/audio_voice_rss.mp3" -c copy -shortest -map 0:v0 -map 1:a:0 "video_outputs/generated_video_with_sound.mp4"
+ffmpeg -i "model/outputs/generated_video_step1.mp4" -i "model/outputs/soundFile.mp3" -c copy -shortest -map 0:v0 -map 1:a:0 "model/outputs/generated_video_step2.mp4"
