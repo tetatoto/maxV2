b602651e0f9fd1f151be12375db5e7175abe3d27
DEBAUCHE Theophile DTSI/DSI <LJSR3076@orange.com>
2017-08-25 10:58:01 +0200

    get option added for Jarvis

--- controler/video_view_articles.php
+++ controler/video_view_articles.php
@@ -4,7 +4,23 @@ include_once('model/getArticleList.php');
 include_once('model/getPageContent.php');
 include_once('model/simple_html_dom.php');
 include_once('model/createQuery.php');
-$searchContent = $_POST['searchContent'];
+// $searchContent = $_POST['searchContent'];
+
+
+// $searchContent will take the POST value if it is set trough the web interface, 
+// and the GET value is this is a call from Jarvisjarvis
+$searchContent = "";
+if (isset($_POST['searchContent'])) {
+    $searchContent = $_POST['searchContent'];
+}
+elseif (isset($_GET['searchContent'])) {
+    $searchContent = $_GET['searchContent'];
+} 
+else {
+    $searchContent = "";
+}
+
+
 $maxArticles = 10;
 $articles = getArticleList($searchContent, $maxArticles);
 $nbArticles = count($articles);
