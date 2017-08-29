40cd86bbf7b84e46e6a48c4a72fa6e65b7c02332
tetatoto <tetatoto.142857@gmail.com>
2017-08-29 14:09:58 +0200

    api yt

--- youtubeAPI.php
+++ youtubeAPI.php
@@ -5,10 +5,12 @@ require('vendor/autoload.php');
 
 // API Youtube data
 
+$apiKey = "AIzaSyCM4hG8lmXY8sZDatjaElYZVfroUY2sKnE";
 $clientID =  "971828143374-0d0fn2ao293mlcg99kvo42ibeqg03jcv.apps.googleusercontent.com";
 $clientSecret = "z3V_PosIm0VhLkvUgRsgmcmy";
 
 $client = new Google_Client();
+$client->setDeveloperKey($apiKey);
 $client->setClientId($clientID);
 $client->setCLientSecret($clientSecret);
 $client->setRedirectUri('http://vps441713.ovh.net/maxV2/youtubeAPI.php');
