30605288ae1cfb43d39823a7db919a6689d2cc6e
tetatoto <tetatoto.142857@gmail.com>
2017-08-29 17:26:43 +0200

    testing youtube api with the uplaod fonctionnality

--- youtubeAPI.php
+++ youtubeAPI.php
@@ -19,13 +19,57 @@ $client->setScopes('https://www.googleapis.com/auth/youtube');
 
 $youtube = new Google_Service_YouTube($client);
 
+session_start();
+
 $loginUrl = $client->createAuthUrl();
 
+if (isset($_GET['code'])) {
+    $client->authenticate($_GET['code']);
+    $_SESSION['token'] = $client->getAccessToken();
+}
+
+if (isset($_SESSION['token'])) {
+    $client->setAccessToken($_SESSION['token']);
+}
 
 
+if (client->getAccessToken()) {
+    $snippet = new Google_Service_YouTube_VideoSnippet();
+    $snippet->setTitle('max video');
+    $snippet->setDescription('video de test');
+    $snippet->setCategory(17);
+
+    $status = new Google_Service_YouTube_VideoStatus(); 
+    $status->setPrivacyStatus('private');
+    
+
+    $video = new Google_Service_YouTube_Video();
+    $video->setSnippet($snippet);
+    $video->setStatus($status);
+
+    
+    $client->setDefer(true);
+
+    $request = $youtube->videos->insert('status,snippet', $video);
+
+    // $file = dirname(__DIR__).'video.mp4';
+    $file = 'model/templates/template_man.mp4';
+
+    $media = new Google_Http_MediaFileUpload($client, $request, 'video/*', file_get_contents($file));
+
+    $video = $client->execute($request);
+
+    var_dump($video);
+
+
+
+} else {
+    ?>
+    <h2>Accès interdit</h2>
+    <p>Vous devez <a href="<?= $loginUrl; ?>">autorisez</a>   l'appli a acceder a votre compte youtube</p>
+    <?php
+}
 ?>
 
-<h2>Accès interdit</h2>
 
-<p>Vous devez <a href="<?= $loginUrl; ?>">autorisez</a>   l'appli a acceder a votre compte youtube</p>
 
