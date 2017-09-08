c287fa9f77ac4885058363bb3d7b824159b4bc94
tetatoto <tetatoto.142857@gmail.com>
2017-09-08 15:24:07 +0200

    test

--- model/testPhantom.js
+++ model/testPhantom.js
@@ -16,17 +16,18 @@ page.open(url, function (status) {
     
     console.log("HERE 1");
     setTimeout(function () {
-        page.evaluate(function() {
         console.log("HERE 2");
-        var j=-1;
-        var images = document.getElementsByTagName("a");
-        console.log(images.length);
-        // Scrapping the google result page in order to get the url of the images
-        for(q = 0; q < images.length; q++){
-            console.log("HERE 2");
-            if(images[q].href.indexOf("/imgres?imgurl=http")>0){
-            image_urls[++j]=decodeURIComponent(images[q].href).split(/=|%|&/)[1].split("?imgref")[0];
-            }
+        page.evaluate(function() {
+            console.log("HERE 3");
+            var j=-1;
+            var images = document.getElementsByTagName("a");
+            console.log(images.length);
+            // Scrapping the google result page in order to get the url of the images
+            for(q = 0; q < images.length; q++){
+                console.log("HERE 2");
+                if(images[q].href.indexOf("/imgres?imgurl=http")>0) {
+                    image_urls[++j]=decodeURIComponent(images[q].href).split(/=|%|&/)[1].split("?imgref")[0];
+                }
         }
 
         // page is redirecting.
