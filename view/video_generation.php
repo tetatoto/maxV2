279209706c1a5143e527bdfdd61a82c2b49866fc
tetatoto <tetatoto.142857@gmail.com>
2017-09-13 15:01:35 +0200

    test

--- view/video_generation.php
+++ view/video_generation.php
@@ -106,7 +106,7 @@
                 <label for="genderChoice">Please choose the gender of your presentator</label><br>
                 <div class="btn-group" data-toggle="buttons" id="genderChoice">
                   <label class="btn btn-primary active">
-                    <input type="radio" name="male" id="option1" autocomplete="off" value="male" checked> Male (Bob)
+                    <input type="radio" name="male" id="option1" autocomplete="off" value="male"> Male (Bob)
                   </label>
                   <label class="btn btn-primary">
                     <input type="radio" name="female" id="option2" autocomplete="off" value="female"> Female (Alice)
