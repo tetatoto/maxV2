19329484601863f0bc36b94a8b1173df71070302
tetatoto <tetatoto.142857@gmail.com>
2017-09-13 11:50:14 +0200

    debug

--- controler/video_result.php
+++ controler/video_result.php
@@ -92,18 +92,18 @@ else {
 
 
 // ADDING THE INTRO
-// $videosToConcatenate = array();
-// array_push($videosToConcatenate, "file 'templates/intro_resized.mp4'");
-// array_push($videosToConcatenate, "file 'outputs/".$videoResultName."'");
+$videosToConcatenate = array();
+array_push($videosToConcatenate, "file 'templates/intro_resized.mp4'");
+array_push($videosToConcatenate, "file 'outputs/".$videoResultName."'");
 
-// $videoResultName2 = "final_with_intro.mp4";
+$videoResultName2 = "final_with_intro.mp4";
 
-// $concatenateLogs = concatenateVideos($videosToConcatenate, $videoResultName2);
+$concatenateLogs = concatenateVideos($videosToConcatenate, $videoResultName2);
 
 // ADDING ZOOM (NOT YET)
 
 // depending on the preocess :
-$finalVideoName = $videoResultName;
+$finalVideoName = $videoResultName2;
 
 
 // Displaying the view :
